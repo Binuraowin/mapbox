b8eadb3c0dd984f6d2d9b4cb7a6781231ab0c969
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2020-11-15 01:08:33 +0530

    get current location

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 mapboxgl.accessToken = '';
+
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+    enableHighAccuracy: true
+  })
+
+  function successLocation(position) {
+    console.log(position)
+  }
+  
+  function errorLocation() {
+   
+  }
+  
 var map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/streets-v11'
